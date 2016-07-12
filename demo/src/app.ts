8c2296bcd8e7e5366bba84bb0ab82e460947d450
Hunter Jansen <rawkamatic@gmail.com>
2016-07-11 23:48:35 -0400

    Add basic options
    Input, output, selectAll, groupBy

--- demo/src/app.ts
+++ demo/src/app.ts
@@ -22,10 +22,16 @@ export class DemoAppComponent {
   demo1: Array<any>;
   demo2: Array<any>;
   demo3: Array<any>;
+  demoVals: Object;
 
   constructor() {
-    this.demo1 = ['a', 'n'];
+    this.demo1 = [{ id: 1, name: "Rose Tyler", type: "companion" }, { id: 2, name: "Donna Noble", type: "companion" }, { id: 3, name: "Martha Jones", type: "companion" }, { id: 4, name: "Dalek", type: "enemy" }];
     this.demo2 = [{}];
     this.demo3 = [{}];
   }
+
+  public refreshValues(value: any): void {
+    this.demoVals = value;
+  }
+
 };
--- src/raw-multiselect.component.ts
+++ src/raw-multiselect.component.ts
@@ -4,7 +4,10 @@ import {
   Inject,
   ApplicationRef,
   ViewContainerRef,
-  Input
+  Input,
+  Output,
+  EventEmitter,
+  OnInit
 } from "@angular/core";
 
 
@@ -18,11 +21,73 @@ import {
   providers: []
 })
 
-export class MultiSelectComponent {
+export class MultiSelectComponent implements OnInit {
   @Input() inbound: Array<any>;
-  @Input() outbound: Array<any>;
+  @Input() displayKey: String;
+  @Input() allSelected: Boolean;
+  @Input() groupBy: string;
+
+  @Output() outbound: EventEmitter<Array<any>> = new EventEmitter();
+
+  groups: Array<any>;
 
   constructor() {
+  }
 
+  getSelectedItems(items): Array<any> {
+    let selectedVals = [];
+    if (this.inbound.length > 0) {
+      this.inbound.forEach(object => {
+        if (object.selected) {
+          selectedVals.push(object);
+        }
+      });
+    }
+    return selectedVals;
+  }
+
+  toggleSelection(item) {
+    item.selected = !item.selected;
+    this.notifyParent();
+  }
+
+  notifyParent() {
+    this.outbound.emit(this.getSelectedItems(this.inbound));
+  }
+
+  selectAll() {
+    this.groups.forEach(object => {
+      object.selected = false;
+    });
+    this.inbound.forEach(object => {
+      object.selected = true;
+    });
+    this.notifyParent();
+  }
+
+  selectNone() {
+    this.groups.forEach(object => {
+      object.selected = false;
+    });
+    this.inbound.forEach(object => {
+      object.selected = false;
+    });
+    this.notifyParent();
+  }
+
+  createGroups() {
+    this.groups = [];
+    this.inbound.forEach(item => {
+      if (this.groups.indexOf(item[this.groupBy].toLowerCase()) === -1) {
+        this.groups.push({ name: item[this.groupBy].toLowerCase(), selected: false });
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.createGroups();
+    if (this.allSelected) {
+      this.selectAll();
+    }
   }
 };
