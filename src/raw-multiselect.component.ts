f7cd0cac0ce241aac6098e94a3cf991172c35ab7
Hunter Jansen <rawkamatic@gmail.com>
2016-07-17 19:27:55 -0400

    add rest of base logic for selecting stuff
    wheee

--- src/raw-multiselect.component.ts
+++ src/raw-multiselect.component.ts
@@ -38,7 +38,7 @@ export class MultiSelectComponent implements OnInit {
     let selectedVals = [];
     if (this.inbound.length > 0) {
       this.inbound.forEach(object => {
-        if (object.selected) {
+        if (object.rawMSSelected) {
           selectedVals.push(object);
         }
       });
@@ -46,8 +46,21 @@ export class MultiSelectComponent implements OnInit {
     return selectedVals;
   }
 
-  toggleSelection(item) {
-    item.selected = !item.selected;
+  toggleSelection(item, event) {
+    if (item.rawMSIsGroup) {
+      item.rawMSSelected = !item.rawMSSelected;
+      this.inbound.forEach(subItem => {
+        if (subItem[this.groupBy] === item.rawMSName) {
+          this.setItemSelected(subItem, item.rawMSSelected);
+        }
+      });
+    } else {
+      item.rawMSSelected = !item.rawMSSelected;
+      if (this.groups.length > 0) {
+        this.checkGroupSelected(item[this.groupBy]);
+      }
+      event.stopPropagation();
+    }
     this.notifyParent();
   }
 
@@ -55,34 +68,62 @@ export class MultiSelectComponent implements OnInit {
     this.outbound.emit(this.getSelectedItems(this.inbound));
   }
 
+  checkGroupSelected(groupName) {
+    let group = this.groups.filter(item => item.rawMSName === groupName)[0];
+
+    let noCount = this.inbound.filter(item => item[this.groupBy] === groupName)
+      .reduce(function (count, item) {
+        return count + !item.rawMSSelected | 0;
+      }, 0);
+    group.rawMSSelected = noCount === 0;
+  }
+
+  setItemSelected(item, selected) {
+    item.rawMSSelected = selected;
+  }
+
+  selectItem(item) {
+    item.rawMSSelected = true;
+  }
+
+  deselectItem(item) {
+    item.rawMSSelected = false;
+  }
+
   selectAll() {
     this.groups.forEach(object => {
-      object.selected = false;
+      object.rawMSSelected = true;
     });
     this.inbound.forEach(object => {
-      object.selected = true;
+      object.rawMSSelected = true;
     });
     this.notifyParent();
   }
 
   selectNone() {
     this.groups.forEach(object => {
-      object.selected = false;
+      object.rawMSSelected = false;
     });
     this.inbound.forEach(object => {
-      object.selected = false;
+      object.rawMSSelected = false;
     });
     this.notifyParent();
   }
 
   createGroups() {
     this.groups = [];
+    let groupVals = [];
     this.inbound.forEach(item => {
-      if (this.groups.indexOf(item[this.groupBy].toLowerCase()) === -1) {
-        this.groups.push({ name: item[this.groupBy].toLowerCase(), selected: false });
+      if (groupVals.indexOf(item[this.groupBy].toLowerCase()) === -1) {
+        groupVals.push(item[this.groupBy].toLowerCase());
       }
     });
+
+    groupVals.forEach(group => {
+      this.groups.push({ rawMSName: group, rawMSSelected: false, rawMSIsGroup: true });
+    });
   }
+
 
   ngOnInit() {
     this.createGroups();
