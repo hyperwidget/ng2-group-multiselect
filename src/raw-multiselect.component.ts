000507a74676b438bd5c5c886950ccc9b466ebc7
Hunter Jansen <rawkamatic@gmail.com>
2016-07-28 01:14:02 -0400

    Move css and template into single file

--- src/raw-multiselect.component.ts
+++ src/raw-multiselect.component.ts
@@ -13,6 +13,116 @@ import {
 
 import {FilterPipe} from "./raw-multiselect.pipe";
 
+let styles = `.rawMSButton {
+    border-radius: 4px;
+    background: white;
+    padding: 8px;
+    background-image: linear-gradient(#FFF, #F2F2F2);
+}
+
+.rawMSInput {
+    border-radius: 4px;
+    padding: 8px;
+    background: #FFF;
+    width: 100%;
+}
+
+.rawMSDropDown {
+    position: absolute;
+    background-color: #FFF;
+    z-index: 999;
+    border: 1px solid rgba(0, 0, 0, .15);
+    border-radius: 4px;
+    box-shadow: 0 6px 12px rgba(0, 0, 0, .15);
+}
+
+.rawMSControllerBox, .rawMSOptionsBox {
+    padding: 15px;
+    margin: 0;
+}
+
+.rawMSControllerBox {
+    border-bottom: 1px solid rgba(221, 221, 221, 1);
+}
+
+.rawMSGroup h4 {
+    margin-right: 10px;
+    min-width: 200px;
+    padding: 4px;
+    border-radius: 4px;
+    border: 1px solid #c3c3c3;
+}
+
+h4.option {
+    margin-bottom: 5px;
+}
+
+.rawMSGroup:first-of-type>h4 {
+    margin-top: 0;
+}
+
+.option {
+    cursor: pointer;
+}
+
+.option.selected {
+    background-color: rgba(214, 214, 214, 1)
+}
+
+.option.listItem {
+    display: inline-block;
+    margin-right: 10px;
+    min-width: 200px;
+    padding: 4px;
+    border-radius: 4px;
+    border: 1px solid #c3c3c3;
+}
+
+
+/*Utility Classes*/
+
+.rawMSRight {
+    float: right;
+}
+
+.rawMSLeft {
+    float: left;
+}`;
+
+let template = `<button (click)="dropDownVisible=!dropDownVisible;" class="rawMSButton">
+    <template ngIf="selectedItems.length > 0">
+        <span *ngFor="let val of selectedItems; let isLast=last">
+                {{val[displayKey]}}{{isLast ? '&#9660;' : ', '}}
+        </span>
+    </template>
+    <span *ngIf="selectedItems.length === 0">None Selected</span>
+</button>
+<div *ngIf="dropDownVisible" class="rawMSDropDown">
+    <div class="rawMSControllerBox">
+        <div>
+            <input type="search" [(ngModel)]="filterVal" class="rawMSInput" placeholder="Filter Options">
+        </div>
+        <div>
+            <button (click)="selectAll();" class="rawMSButton">All</button>
+            <button (click)="selectNone();" class="rawMSButton">None</button>
+        </div>
+    </div>
+    <div *ngIf="inbound.length > 0" class="rawMSOptionsBox">
+        <div *ngFor="let group of groups" (click)="toggleSelection(group);" class="rawMSGroup">
+            <h4 class="option" [ngClass]="{selected: group.rawMSSelected}" *ngIf="groups[0].name!=='rawMSPlaceHolderGroup';">{{group.rawMSName}}
+                <span class="rawMSRight" *ngIf="group.rawMSSelected">&#10003;</span>
+            </h4>
+            <template ngFor let-option [ngForOf]="inbound | filter:filterVal:displayKey">
+                <div *ngIf="option[groupBy] === group.rawMSName || groups[0]['displayKey']==='rawMSPlaceHolderGroup';" (click)="toggleSelection(option, $event);"
+                    class="option listItem" [ngClass]="{selected: option.rawMSSelected}">
+                    {{option[displayKey]}}
+                    <span class="rawMSRight" *ngIf="option.rawMSSelected">&#10003;</span>
+                </div>
+            </template>
+        </div>
+    </div>
+</div>`;
+
 @Component({
   selector: "raw-multiselect",
   directives: [],
@@ -21,8 +131,8 @@ import {FilterPipe} from "./raw-multiselect.pipe";
   },
   // Global styles imported in the app component.
   encapsulation: ViewEncapsulation.None,
-  styles: [require("./raw-multiselect.component.css")],
-  template: require("./raw-multiselect.component.html"),
+  styles: [styles],
+  template: template,
   pipes: [FilterPipe]
 })
 
